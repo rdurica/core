420838f9bfdc6a95c487ea7a629375e88511525f
Robert Durica <r.durica@gmail.com>
2023-08-20 21:23:40 +0200

    Get request parameter with datatype

--- src/Presenter/Presenter.php
+++ src/Presenter/Presenter.php
@@ -31,4 +31,56 @@ abstract class Presenter extends NettePresenter
         $this->getUser()->logout(true);
         $this->getPresenter()->redirect('Sign:in');
     }
+
+    /**
+     * Get request parameter value as string.
+     *
+     * @param string $name
+     * @return string|null
+     */
+    final public function getStringParameter(string $name): ?string
+    {
+        return $this->getParameter($name);
+    }
+
+    /**
+     * Get request parameter value as integer.
+     *
+     * @param string $name
+     * @return int|null
+     */
+    final public function getIntParameter(string $name): ?int
+    {
+        return $this->getParameterValue($name, FILTER_VALIDATE_INT);
+    }
+
+    /**
+     * Get request parameter value as boolean.
+     *
+     * @param string $name
+     * @return bool|null
+     */
+    final public function getBooleanParameter(string $name): ?bool
+    {
+        return $this->getParameterValue($name, FILTER_VALIDATE_BOOLEAN);
+    }
+
+    /**
+     * Validate and get value from request.
+     *
+     * @param string $parameter
+     * @param int    $filter
+     * @return int|string|bool|null
+     */
+    private function getParameterValue(string $parameter, int $filter): null|int|string|bool
+    {
+        if ($parameter === '') {
+            return null;
+        }
+
+        $param = $this->getParameter($parameter);
+        $result = filter_var($param, $filter, FILTER_NULL_ON_FAILURE);
+
+        return $result ?: null;
+    }
 }
