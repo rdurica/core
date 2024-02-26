a74e893d20490768d1825780bc2d16e8e2c8f153
Robert Durica <r.durica@gmail.com>
2024-02-26 21:44:16 +0100

    DataGrid templates

---
+++ src/Component/DataGrid.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Rdurica\Core\Component\Component;
+
+use Ublaboo\DataGrid\DataGrid as UblabooDataGrid;
+
+/**
+ * DataGrid.
+ *
+ * @package   src\Component
+ * @copyright Copyright (c) 2024, Robert Durica
+ * @since     2024-02-26
+ */
+final class DataGrid extends UblabooDataGrid
+{
+}
